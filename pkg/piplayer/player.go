5f66991d036872ff4f1bd521d13bc6ca47e72476
thatguycleeb <16028559+thatguycleeb@users.noreply.github.com>
2026-03-26 11:24:58 +0200

    added flag for clearing cache aggressively

--- pkg/piplayer/player.go
+++ pkg/piplayer/player.go
@@ -166,7 +166,8 @@ func (p *Player) startBrowser() error {
 		"--enable-features=UseOzonePlatform",
 		"--ozone-platform=wayland",
 		"--autoplay-policy=no-user-gesture-required",
-		"--disk-cache-dir=/dev/null", //in theory this clears the chrome cache
+		"--disk-cache-dir=/dev/null", //this sets the cache store location to null 
+		"--aggressive-cache-discard", //in theory this clears the chrome cache
 		"http://localhost:8080/viewer",
 	}
 
